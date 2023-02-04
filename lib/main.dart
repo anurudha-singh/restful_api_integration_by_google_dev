d90bfeb249fb540c445aafd9b5bb9fa4518cd397
Lucifer <singhanurudh570@gmail.com>
2023-02-04 17:18:41 +0530

    project blog link added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:restful_api_google/screens/home.dart';
-
+//refer this blog for understanding the complete code
+//https://medium.com/google-developer-experts/working-with-rest-apis-flutter-f3c8c673f1dc
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
